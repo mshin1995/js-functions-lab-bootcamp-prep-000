6b5fec73d037acd4f0efbd02e7b277f34eb224a8
lucky-reducer-8961 <mshin1995@gmail.com>
2019-01-21 03:55:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown() {
-  return `It
+function holidayCountdown(days, holiday) {
+  return `It\'s ${days} days until ${holiday}!`
 }
